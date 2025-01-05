6cc24f015ff5b43bf97889ad58eccc03b0aa77d7
jgabrielleal <joaogabrieel79@gmail.com>
2025-01-05 02:07:25 +0000

    feat: (painel) Finalizando interface do navbar

--- app/Livewire/Panel/Dashboard.php
+++ app/Livewire/Panel/Dashboard.php
@@ -3,11 +3,23 @@
 namespace App\Livewire\Panel;
 
 use Livewire\Attributes\Layout;
+use Livewire\Attributes\Title;
 use Livewire\Component;
+
+use Illuminate\Support\Facades\Auth;
 
 class Dashboard extends Component
 {
     #[Layout('components.layouts.panel')]
+    #[Title('Dashboard')]
+
+    public function mount()
+    {
+        if (!Auth::check()) {
+            return $this->redirect('/painel', navigate: true);
+        }
+    }
+
     public function render()
     {
         return view('livewire.panel.dashboard');
--- resources/views/components/layouts/panel.blade.php
+++ resources/views/components/layouts/panel.blade.php
@@ -1,12 +1,70 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        @include('components.partials.header')
-        <!-- Native CSS -->
-        <link rel="stylesheet" href="{{ asset('css/panel.css') }}">
-    </head>
-    <body class="panel-body">
-        {{ $slot }}
-        @include('components.partials.scripts')
-    </body>
+
+<head>
+    @include('components.partials.header')
+    <!-- Native CSS -->
+    <link rel="stylesheet" href="{{ asset('css/panel.css') }}">
+</head>
+
+<body class="panel-body">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/favicon.ico') }}" alt="Logo" class="logo">
+            </a>
+            <button class="navbar-toggler border-0 shadow-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-3 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active text-uppercase fst-italic d-flex gap-1" href="#">
+                            <img src="{{ asset('icons/materias.svg') }}" alt="Matérias" />Matérias
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-uppercase fst-italic d-flex gap-1" href="#">
+                            <img src="{{ asset('icons/locucao.svg') }}" alt="Locução" />Locução
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-uppercase fst-italic d-flex gap-1" href="#">
+                            <img src="{{ asset('icons/radio.svg') }}" alt="Rádio" />Rádio
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-uppercase fst-italic d-flex gap-1" href="#">
+                            <img src="{{ asset('icons/podcast.svg') }}" alt="Podcasts" />Podcasts
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-uppercase fst-italic d-flex gap-1" href="#">
+                            <img src="{{ asset('icons/marketing.svg') }}" alt="Marketing" />Marketing
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-uppercase fst-italic d-flex gap-1" href="#">
+                            <img src="{{ asset('icons/adms.svg') }}" alt="ADM's" />ADM's
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-uppercase fst-italic d-flex gap-1" href="#">
+                            <img src="{{ asset('icons/logs.svg') }}" alt="Logs's" />Logs's
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-uppercase fst-italic d-flex gap-1" href="#">
+                            <img src="{{ asset('icons/perfil.svg') }}" alt="Perfil" />Perfil
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    {{ $slot }}
+    @include('components.partials.scripts')
+</body>
+
 </html>
