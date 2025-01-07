f182c8d7060a82c0f678375640a75a56ce65b7d6
jgabrielleal <joaogabrieel79@gmail.com>
2025-01-07 01:57:16 +0000

    feat: Criando interface para ações rápidas

--- app/Livewire/Panel/Inline/Dashboard/AcoesRapidas.php
+++ app/Livewire/Panel/Inline/Dashboard/AcoesRapidas.php
@@ -11,9 +11,29 @@ class AcoesRapidas extends Component
         return <<<'HTML'
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xxl-12">
-                <section class="acoes-rapidas">
+                <section class="acoes-rapidas mb-4">
                     <div class="title-default">
                         <h1>Ações Rápidas</h1>
+                    </div>
+                    <div class="acoes-rapidas-carrousel d-flex gap-2 mt-1">
+                        <button type="button" class="btn d-flex gap-1">
+                            <i class="bi bi-layers-fill"></i>Nova Matéria
+                        </button>
+                        <button type="button" class="btn d-flex gap-1">
+                            <i class="bi bi-bell-fill"></i>Criar Avisos
+                        </button>
+                        <button type="button" class="btn d-flex gap-1">
+                            <i class="bi bi-cloud-upload-fill"></i>Deixar Arquivos
+                        </button>
+                        <button type="button" class="btn d-flex gap-1">
+                            <i class="bi bi-mic-fill"></i>Iniciar Programa
+                        </button>
+                        <button type="button" class="btn d-flex gap-1">
+                            <i class="bi bi-calendar-day-fill"></i>Adicionar Eventos
+                        </button>
+                        <button type="button" class="btn d-flex gap-1">
+                            <i class="bi bi-music-note-list"></i>Caixa de Pedidos
+                        </button>
                     </div>
                 </section>
             </div>
