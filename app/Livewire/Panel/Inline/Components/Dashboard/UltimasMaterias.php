50021ac50a7f364e1f8931284190b0240a1d6cbd
jgabrielleal <joaogabrieel79@gmail.com>
2025-01-19 23:25:52 +0000

    feat: Criando o bloco ultimas matérias no dashboard

--- app/Livewire/Panel/Inline/Components/Dashboard/UltimasMaterias.php
+++ app/Livewire/Panel/Inline/Components/Dashboard/UltimasMaterias.php
@@ -15,6 +15,153 @@ class UltimasMaterias extends Component
                     <div class="title-default">
                         <h1>Últimas matérias</h1>
                     </div>
+                    <div class="ultimas-materias-carrousel gap-2">
+                        <div class="ultimas-materias-item">
+                            <p class="ultimas-materias-titulo">
+                                It is a long established fact that a reader 
+                                will be distracted by the readable content 
+                                of a page when looking at its layout. The point 
+                                of using Lorem Ipsum is that it has a more-or-less 
+                                normal distribution of letters, as opposed to 
+                                using 'Content here, content here', making it look 
+                                like readable English. Many desktop publishing packages 
+                                and web page editors now use Lorem Ipsum as their 
+                                default model text, and a search for 'lorem ipsum' will 
+                                uncover many web sites still in their infancy. Various 
+                                versions have evolved over the years, sometimes by accident, 
+                                sometimes on purpose (injected humour and the like).
+                            </p>
+                            <div class="ultimas-materias-autor d-flex justify-content-between">
+                                <span class="d-block text-uppercase">
+                                    Neko Kirame
+                                </span>
+                                <div class="d-flex gap-2">
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/visualizar.svg') }}" alt="Editar"/>
+                                    </a>
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/editar.svg') }}" alt="Editar"/>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ultimas-materias-item">
+                            <p class="ultimas-materias-titulo">
+                                It is a long established fact that a reader 
+                                will be distracted by the readable content 
+                                of a page when looking at its layout. The point 
+                                of using Lorem Ipsum is that it has a more-or-less 
+                                normal distribution of letters, as opposed to 
+                                using 'Content here, content here', making it look 
+                                like readable English. Many desktop publishing packages 
+                                and web page editors now use Lorem Ipsum as their 
+                                default model text, and a search for 'lorem ipsum' will 
+                                uncover many web sites still in their infancy. Various 
+                                versions have evolved over the years, sometimes by accident, 
+                                sometimes on purpose (injected humour and the like).
+                            </p>
+                            <div class="ultimas-materias-autor d-flex justify-content-between">
+                                <span class="d-block text-uppercase">
+                                    Neko Kirame
+                                </span>
+                                <div class="d-flex gap-2">
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/visualizar.svg') }}" alt="Editar"/>
+                                    </a>
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/editar.svg') }}" alt="Editar"/>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ultimas-materias-item">
+                            <p class="ultimas-materias-titulo">
+                                It is a long established fact that a reader 
+                                will be distracted by the readable content 
+                                of a page when looking at its layout. The point 
+                                of using Lorem Ipsum is that it has a more-or-less 
+                                normal distribution of letters, as opposed to 
+                                using 'Content here, content here', making it look 
+                                like readable English. Many desktop publishing packages 
+                                and web page editors now use Lorem Ipsum as their 
+                                default model text, and a search for 'lorem ipsum' will 
+                                uncover many web sites still in their infancy. Various 
+                                versions have evolved over the years, sometimes by accident, 
+                                sometimes on purpose (injected humour and the like).
+                            </p>
+                            <div class="ultimas-materias-autor d-flex justify-content-between">
+                                <span class="d-block text-uppercase">
+                                    Neko Kirame
+                                </span>
+                                <div class="d-flex gap-2">
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/visualizar.svg') }}" alt="Editar"/>
+                                    </a>
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/editar.svg') }}" alt="Editar"/>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ultimas-materias-item">
+                            <p class="ultimas-materias-titulo">
+                                It is a long established fact that a reader 
+                                will be distracted by the readable content 
+                                of a page when looking at its layout. The point 
+                                of using Lorem Ipsum is that it has a more-or-less 
+                                normal distribution of letters, as opposed to 
+                                using 'Content here, content here', making it look 
+                                like readable English. Many desktop publishing packages 
+                                and web page editors now use Lorem Ipsum as their 
+                                default model text, and a search for 'lorem ipsum' will 
+                                uncover many web sites still in their infancy. Various 
+                                versions have evolved over the years, sometimes by accident, 
+                                sometimes on purpose (injected humour and the like).
+                            </p>
+                            <div class="ultimas-materias-autor d-flex justify-content-between">
+                                <span class="d-block text-uppercase">
+                                    Neko Kirame
+                                </span>
+                                <div class="d-flex gap-2">
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/visualizar.svg') }}" alt="Editar"/>
+                                    </a>
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/editar.svg') }}" alt="Editar"/>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="ultimas-materias-item">
+                            <p class="ultimas-materias-titulo">
+                                It is a long established fact that a reader 
+                                will be distracted by the readable content 
+                                of a page when looking at its layout. The point 
+                                of using Lorem Ipsum is that it has a more-or-less 
+                                normal distribution of letters, as opposed to 
+                                using 'Content here, content here', making it look 
+                                like readable English. Many desktop publishing packages 
+                                and web page editors now use Lorem Ipsum as their 
+                                default model text, and a search for 'lorem ipsum' will 
+                                uncover many web sites still in their infancy. Various 
+                                versions have evolved over the years, sometimes by accident, 
+                                sometimes on purpose (injected humour and the like).
+                            </p>
+                            <div class="ultimas-materias-autor d-flex justify-content-between">
+                                <span class="d-block text-uppercase">
+                                    Neko Kirame
+                                </span>
+                                <div class="d-flex gap-2">
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/visualizar.svg') }}" alt="Editar"/>
+                                    </a>
+                                    <a href="#">
+                                        <img src="{{ asset('icons/panel/editar.svg') }}" alt="Editar"/>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
