152d71c189e752ee457c7dd7b1d958184b8f4197
jgabrielleal <joaogabrieel79@gmail.com>
2025-01-23 01:17:37 +0000

    feat: Interface do calendario

--- app/Livewire/Panel/Inline/Components/Dashboard/Calendario.php
+++ app/Livewire/Panel/Inline/Components/Dashboard/Calendario.php
@@ -15,6 +15,86 @@ class Calendario extends Component
                     <div class="title-default">
                         <h1>Calendário</h1>
                     </div>
+                    <div class="calendario-tags d-flex gap-2">
+                        <div class="calendario-tags-item text-uppercase">
+                            Programas
+                        </div>
+                        <div class="calendario-tags-item text-uppercase">
+                            Lives
+                        </div>
+                        <div class="calendario-tags-item text-uppercase">
+                            Youtube
+                        </div>
+                        <div class="calendario-tags-item text-uppercase">
+                            Podcast
+                        </div>
+                        <div class="calendario-tags-item text-uppercase">
+                        </div>
+                        <div class="calendario-tags-item text-uppercase">
+                        </div>                        
+                        <div class="calendario-tags-item text-uppercase">
+                        </div>
+                    </div>
+                    <div class="calendario-dias d-flex gap-2 mt-4">
+                        <div class="calendario-dias-item text-center text-uppercase d-block">Dom</div>
+                        <div class="calendario-dias-item text-center text-uppercase d-block">Seg</div>
+                        <div class="calendario-dias-item text-center text-uppercase d-block">Ter</div>
+                        <div class="calendario-dias-item text-center text-uppercase d-block">Qua</div>
+                        <div class="calendario-dias-item text-center text-uppercase d-block">Qui</div>
+                        <div class="calendario-dias-item text-center text-uppercase d-block">Sex</div>
+                        <div class="calendario-dias-item text-center text-uppercase d-block">Sab</div>
+                    </div>
+                    <div class="calendario-tarefas d-flex gap-2 mt-3">
+                        <div class="calendario-tarefas-coluna">
+                            <div class="calendario-tarefas-item">
+                                <span>08H00</span>
+                                <h1>Revolução Anime</h1>
+                                <span>Roger Ventura</span>
+                            </div>
+                        </div>
+                        <div class="calendario-tarefas-coluna">
+                            <div class="calendario-tarefas-item">
+                                <span>08H00</span>
+                                <h1>Revolução Anime</h1>
+                                <span>Roger Ventura</span>
+                            </div>
+                        </div>
+                        <div class="calendario-tarefas-coluna">
+                            <div class="calendario-tarefas-item">
+                                <span>08H00</span>
+                                <h1>Revolução Anime</h1>
+                                <span>Roger Ventura</span>
+                            </div>
+                        </div>
+                        <div class="calendario-tarefas-coluna">
+                            <div class="calendario-tarefas-item">
+                                <span>08H00</span>
+                                <h1>Revolução Anime</h1>
+                                <span>Roger Ventura</span>
+                            </div>
+                        </div>
+                        <div class="calendario-tarefas-coluna">
+                            <div class="calendario-tarefas-item">
+                                <span>08H00</span>
+                                <h1>Revolução Anime</h1>
+                                <span>Roger Ventura</span>
+                            </div>
+                        </div>
+                        <div class="calendario-tarefas-coluna">
+                            <div class="calendario-tarefas-item">
+                                <span>08H00</span>
+                                <h1>Revolução Anime</h1>
+                                <span>Roger Ventura</span>
+                            </div>
+                        </div>
+                        <div class="calendario-tarefas-coluna">
+                            <div class="calendario-tarefas-item">
+                                <span>08H00</span>
+                                <h1>Revolução Anime</h1>
+                                <span>Roger Ventura</span>
+                            </div>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
--- resources/views/livewire/panel/dashboard.blade.php
+++ resources/views/livewire/panel/dashboard.blade.php
@@ -4,4 +4,5 @@
     @livewire('panel.inline.components.dashboard.avisos-para-equipe')
     @livewire('panel.inline.components.dashboard.minhas-tarefas')
     @livewire('panel.inline.components.dashboard.ultimas-materias')
+    @livewire('panel.inline.components.dashboard.calendario')
 </div>
