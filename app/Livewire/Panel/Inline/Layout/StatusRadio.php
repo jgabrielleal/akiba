c453263ad17c088ef94ca7b8808aefcf01f0791e
jgabrielleal <joaogabrieel79@gmail.com>
2025-01-24 01:05:48 +0000

    refactor: Reestruturando o scss

---
+++ app/Livewire/Panel/Inline/Layout/StatusRadio.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Panel\Inline\Layout;
+
+use Livewire\Component;
+
+class StatusRadio extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.inline.layout.status-radio');
+    }
+}
